848f00f24b777c13d1d2021fc2e551d7e5ce85ad
vr7bd <varun@zoapi.com>
2023-07-20 20:49:22 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
 
 // Import the native module. On web, it will be resolved to ExpoPlaystoreChecker.web.ts
 // and on native platforms to ExpoPlaystoreChecker.ts
 import ExpoPlaystoreCheckerModule from './ExpoPlaystoreCheckerModule';
-import ExpoPlaystoreCheckerView from './ExpoPlaystoreCheckerView';
-import { ChangeEventPayload, ExpoPlaystoreCheckerViewProps } from './ExpoPlaystoreChecker.types';
 
 // Get the native constant value.
-export const PI = ExpoPlaystoreCheckerModule.PI;
 
-export function hello(): string {
-  return ExpoPlaystoreCheckerModule.hello();
+export function isAppStoreInstalled(): boolean {
+  return ExpoPlaystoreCheckerModule.isAppStoreInstalled();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoPlaystoreCheckerModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoPlaystoreCheckerModule ?? NativeModulesProxy.ExpoPlaystoreChecker);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoPlaystoreCheckerView, ExpoPlaystoreCheckerViewProps, ChangeEventPayload };
